3c733d7dc745add82f9c86913dabac54e6436429
gbfranko <107503088+gbfranko@users.noreply.github.com>
2022-07-14 18:22:46 -0400

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 14 11:37:49 2022
+
+@author: gunnarfranko
+"""
+from .montecarlo import Die
+from .montecarlo import Game
+from .montecarlo import Analyzer
+
+print("Let's Play a Game!")
