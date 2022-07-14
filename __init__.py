402cf910a2675103ba3159ecc40a868b331ccccf
gbfranko <107503088+gbfranko@users.noreply.github.com>
2022-07-14 18:28:39 -0400

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 14 11:37:49 2022
+
+@author: gunnarfranko
+"""
+from .montecarlo_test import montecarloTestSuite
+
+print("Testing")
