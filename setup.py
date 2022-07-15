47d847081823e210e28b13bac1be1428b6e5cced
gbfranko <107503088+gbfranko@users.noreply.github.com>
2022-07-15 11:39:04 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,16 @@ Created on Thu Jul 14 11:37:51 2022
 @author: gunnarfranko
 """
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='montecarlo',
-      version='0.1',
-      description='A die game',
-      url='https://github.com/gbfranko/MonteCarlo-Project',
-      author='Gunnar Franko',
-      author_email='gbf6qz@virginia.edu',
-      license='MIT',
-      packages=['montecarlo'])
+
+setup(
+    name='MonteCarlo',
+    version='1.0.0',
+    url='https://github.com/gbfranko/MonteCarlo-Project',
+    author='Gunnar Franko',
+    author_email='gbf6qz@virginia.edu',
+    description='Runs a die game',
+    packages=find_packages(),    
+    install_requires=['numpy >= 1.11.1', 'matplotlib >= 1.5.1'],
+)
